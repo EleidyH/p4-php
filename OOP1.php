61da76c4237bcc738d62d48f830b3af729afe0a5
EleidyH <144007961+EleidyH@users.noreply.github.com>
2024-06-07 09:29:23 +0200

    oop1

---
+++ OOP1.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Product 
+{
+
+}
+
+
+$chair1 = new Product();
+$chair2 = new Product();
+$chair3 = new Product();
+
+var_dump($chair1);
+var_dump($chair2);
+var_dump($chair3);
+
+?>
